0c71b8f13f34fa6aac5579cd6b0049b0ef553604
sawyer bristol <sawyerbristol@gmail.com>
2023-02-19 12:56:58 -0700

    Update avrdude.sh

--- keyboards/lily58/keymaps/LegitCamper/avrdude.sh
+++ keyboards/lily58/keymaps/LegitCamper/avrdude.sh
@@ -1,3 +1,5 @@
 # For Pro Micro
 #
-sudo avrdude -p m32u4 -c avr109 -P /dev/ttyACM0 -U flash:w:../../../../lily58_rev1_LegitCamper.hex:i 
+stty -F /dev/ttyACM0 speed 1200
+stty -F /dev/ttyACM0 speed 115200
+sudo avrdude -p m32u4 -c avr109 -P /dev/ttyACM0 -U flash:w:../../../../lily58_rev1_LegitCamper.hex:h
